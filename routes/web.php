35ac8adb164b36933dc2c62ad9df13ae99e6a308
hansangwoo <hansangwoos456@gmail.com>
2025-10-21 16:55:58 +0900

    feat : Auth 체크 하여 login or dashboard 로 리다이렉트

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
+
+// 메인 페이지 -> 로그인 또는 대시보드로 이동
+Route::get('/', function(){
+    return Auth::check()
+        ? redirect()->route('dashboard')
+        : redirect()->route('login');
+});
+
+// 로그인 페이지
+Route::get('/login', \App\Livewire\Auth\Login::class)
+    ->name('login')
+    ->middleware('guest');
+
+
+// 인증된 사용자 전용 라우트
+Route::middleware('auth')->group(function () {
+    // 대시보드
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    // 로그아웃
+    Route::post('/logout', function () {
+        Auth::logout();
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+        return redirect()->route('login');
+    })->name('logout');
 });
